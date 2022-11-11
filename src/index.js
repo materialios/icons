aae061a6ec4d1d8096009794ac88b75ded4c86cc
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-11-12 01:09:55 +0700

    update ExpandLessIcon.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import IconWrapper from './IconWrapper'
-
 import ArrowBackIcon from './icons/ArrowBackIcon'
 import MenuIcon from './icons/MenuIcon'
 import ExpandMoreIcon from './icons/ExpandMoreIcon'
+import ExpandLessIcon from './icons/ExpandMoreIcon'
 
 export {
   IconWrapper,
   ArrowBackIcon,
   MenuIcon,
-  ExpandMoreIcon
+  ExpandMoreIcon,
+  ExpandLessIcon
 }
